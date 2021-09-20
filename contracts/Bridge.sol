46c2769d1a7748f71783bdeda7a9f517de44cb65
celenksercan <52419173+celenksercan@users.noreply.github.com>
2021-09-20 23:47:07 +0300

    Update Bridge.sol

--- contracts/Bridge.sol
+++ contracts/Bridge.sol
@@ -2,3 +2,8 @@ pragma solidity ^0.8.0;
 
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 import './Itoken.sol';
+
+contract BridgeBase {
+  address public admin;
+  IToken public token;
+  mapping(address => mapping(uint => bool)) public processedNonces;
