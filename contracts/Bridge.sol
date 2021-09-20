eb16f423221d7dd7bcd5ad8e28922ee4a01f00e2
celenksercan <52419173+celenksercan@users.noreply.github.com>
2021-09-20 23:50:05 +0300

    Update Bridge.sol

--- contracts/Bridge.sol
+++ contracts/Bridge.sol
@@ -104,3 +104,16 @@ function splitSignature(bytes memory sig)
     bytes32 r;
     bytes32 s;
     uint8 v;
+
+ assembly {
+        // first 32 bytes, after the length prefix
+        r := mload(add(sig, 32))
+        // second 32 bytes
+        s := mload(add(sig, 64))
+        // final byte (first byte of the next 32 bytes)
+        v := byte(0, mload(add(sig, 96)))
+    }
+  
+    return (v, r, s);
+  }
+}
