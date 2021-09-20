e7303c246639208db2e1f24b301bf79e28d8044a
celenksercan <52419173+celenksercan@users.noreply.github.com>
2021-09-20 23:49:10 +0300

    Update Bridge.sol

--- contracts/Bridge.sol
+++ contracts/Bridge.sol
@@ -70,3 +70,12 @@ emit Transfer(
       signature,
       Step.Mint
     );
+
+}
+
+  function prefixed(bytes32 hash) internal pure returns (bytes32) {
+    return keccak256(abi.encodePacked(
+      '\x19Ethereum Signed Message:\n32', 
+      hash
+    ));
+  }
