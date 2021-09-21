655e6dc13ea81c0a8cdc08478520fe3ced3e33cb
celenksercan <52419173+celenksercan@users.noreply.github.com>
2021-09-21 23:36:54 +0300

    Create tokenBase.sol

---
+++ contracts/tokenBase.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.8.0;
+
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+
+contract TokenBase is ERC20 {
+  address public admin;
+
+  constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+    admin = msg.sender;
+  }
