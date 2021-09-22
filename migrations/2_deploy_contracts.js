f261371191e8602d680ec8bcf9bbd9e15029d5a3
celenksercan <52419173+celenksercan@users.noreply.github.com>
2021-09-22 23:52:22 +0300

    Create 2_deploy_contracts.js

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,4 @@
+const tokenEth = artifacts.require('tokenEth.sol');
+const tokenBsc = artifacts.require('tokenBsc.sol');
+const ethBridge = artifacts.require('ethBridge.sol');
+const bscBridge = artifacts.require('bscBridge.sol');
