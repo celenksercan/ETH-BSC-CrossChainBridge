2b203530da0f92cc99bb554def94934958a27a77
celenksercan <52419173+celenksercan@users.noreply.github.com>
2021-09-22 23:56:26 +0300

    Update ethTokenBalance.js

--- scripts/ethTokenBalance.js
+++ scripts/ethTokenBalance.js
@@ -6,3 +6,14 @@ const web3Eth = new Web3('url to eth node (websocket)');
 const web3Bsc = new Web3('https://data-seed-prebsc-1-s1.binance.org:8545');
 const adminPrivKey = '';
 const { address: admin } = web3Bsc.eth.accounts.wallet.add(adminPrivKey);
+
+const bridgeEth = new web3Eth.eth.Contract(
+  BridgeEth.abi,
+  BridgeEth.networks['4'].address
+);
+
+const bridgeBsc = new web3Bsc.eth.Contract(
+  BridgeBsc.abi,
+  BridgeBsc.networks['97'].address
+);
+
