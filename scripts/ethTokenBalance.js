ae09c04e78ee0a2bc063056f78b27300bd97a4ea
celenksercan <52419173+celenksercan@users.noreply.github.com>
2021-09-22 23:56:42 +0300

    Update ethTokenBalance.js

--- scripts/ethTokenBalance.js
+++ scripts/ethTokenBalance.js
@@ -17,3 +17,33 @@ const bridgeBsc = new web3Bsc.eth.Contract(
   BridgeBsc.networks['97'].address
 );
 
+bridgeEth.events.Transfer(
+  {fromBlock: 0, step: 0}
+)
+.on('data', async event => {
+  const { from, to, amount, date, nonce, signature } = event.returnValues;
+
+  const tx = bridgeBsc.methods.mint(from, to, amount, nonce, signature);
+  const [gasPrice, gasCost] = await Promise.all([
+    web3Bsc.eth.getGasPrice(),
+    tx.estimateGas({from: admin}),
+  ]);
+  const data = tx.encodeABI();
+  const txData = {
+    from: admin,
+    to: bridgeBsc.options.address,
+    data,
+    gas: gasCost,
+    gasPrice
+  };
+  const receipt = await web3Bsc.eth.sendTransaction(txData);
+  console.log(`Transaction hash: ${receipt.transactionHash}`);
+  console.log(`
+    Processed transfer:
+    - from ${from} 
+    - to ${to} 
+    - amount ${amount} tokens
+    - date ${date}
+    - nonce ${nonce}
+  `);
+});
