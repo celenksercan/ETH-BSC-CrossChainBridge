202734f4219a896229e9413d3312674aea7a23cb
celenksercan <52419173+celenksercan@users.noreply.github.com>
2021-09-22 23:59:37 +0300

    Update eth-bsc-Transfer.js

--- scripts/eth-bsc-Transfer.js
+++ scripts/eth-bsc-Transfer.js
@@ -3,7 +3,7 @@ const BridgeEth = artifacts.require('./ethBridge.sol');
 const privKey = 'priv key of sender';
 
 module.exports = async done => {
-  const nonce = 1; //Need to increment this for each new transfer
+  const nonce = 1; // Her yeni transferde 1'er 1'er artar..
   const accounts = await web3.eth.getAccounts();
   const bridgeEth = await BridgeEth.deployed();
   const amount = 1000;
@@ -13,3 +13,12 @@ module.exports = async done => {
     {t: 'uint256', v: amount},
     {t: 'uint256', v: nonce},
   ).toString('hex');
+
+  
+   const { signature } = web3.eth.accounts.sign(
+    message, 
+    privKey
+  ); 
+  await bridgeEth.burn(accounts[0], amount, nonce, signature);
+  done();
+}
