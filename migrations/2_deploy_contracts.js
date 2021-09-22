3b61f39364d7e00eb381c7edcd3e28d2448c6017
celenksercan <52419173+celenksercan@users.noreply.github.com>
2021-09-22 23:53:21 +0300

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,3 +2,13 @@ const tokenEth = artifacts.require('tokenEth.sol');
 const tokenBsc = artifacts.require('tokenBsc.sol');
 const ethBridge = artifacts.require('ethBridge.sol');
 const bscBridge = artifacts.require('bscBridge.sol');
+
+module.exports = async function (deployer, network, addresses) {
+  if(network === 'ethTestnet') {
+    await deployer.deploy(tokenEth);
+    const tokenEth = await tokenEth.deployed();
+    await tokenEth.mint(addresses[0], 1000);
+    await deployer.deploy(ethBridge, tokenEth.address);
+    const ethBridge = await ethBridge.deployed();
+    await tokenEth.updateAdmin(ethBridge.address);
+  }
